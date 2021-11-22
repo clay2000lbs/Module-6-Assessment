b0fe26f188db3fefc0a7bc76d5a7f9402c57e01b
Clayton  J. Wilkerson <clay2000lbs@gmail.com>
2021-11-21 18:41:58 -0600

    part 3 done

--- assessment-qa-devops/functions.test.ts
+++ assessment-qa-devops/functions.test.ts
@@ -1,5 +1,17 @@
 const {shuffleArray} = require('./utils')
 
+
 describe('shuffleArray should', () => {
     // CODE HERE
+    let bots = [1, 2, 3]
+    let shuffleValue = shuffleArray(bots)
+
+    test('shuffleArray should return an array', () => {
+        expect(typeof shuffleValue).toEqual('array')
+    })
+
+    test('returned array should be the same length as as the argument array', () => {
+        expect(bots.length == shuffleArray.length)
+    })
+
 })
